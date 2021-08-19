024420018a0289d9701bea11da755dbcc7c931ef
Fernando Velasco <fervelgo@gmail.com>
2021-08-19 15:13:47 +0100

    installed missing dependencies

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -2,7 +2,25 @@ const router = require("express").Router();
 const { checkUsernameExists, validateRoleName } = require('./auth-middleware');
 const { JWT_SECRET } = require("../secrets"); // use this secret!
 
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const Users = require('../users/users-model');
+
+function buildToken(user) {
+  const payload = {
+    subject: user.user_id,
+    username: user.username,
+    role_name: user.role_name
+  }
+  const options = {
+    expiresIn: '1d',
+  }
+  return jwt.sign(payload, JWT_SECRET, options)
+}
+
 router.post("/register", validateRoleName, (req, res, next) => {
+ 
   /**
     [POST] /api/auth/register { "username": "anna", "password": "1234", "role_name": "angel" }
 
@@ -18,6 +36,21 @@ router.post("/register", validateRoleName, (req, res, next) => {
 
 
 router.post("/login", checkUsernameExists, (req, res, next) => {
+  let { username, password } = req.body;
+
+  Users.findBy({ username })
+    .then(([user]) => {
+      if(user && bcrypt.compareSync(password, user.password)) {
+        const token = buildToken(user)
+        res.status(200).json({
+          message: `${user.username} is back!`,
+          token : token,
+        });
+      } else {
+        next({ status: 401, message: 'Invalid Credentials' })
+      }
+    })
+  
   /**
     [POST] /api/auth/login { "username": "sue", "password": "1234" }
 
